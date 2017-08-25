0d1dae4f7d6de78e975299bb52fb8da2e8e1ce58
Davorin <davorin.prislin@gmail.com>
2017-08-25 14:04:17 +0200

    Added JS files to gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,15 @@ gulp.task('css', function () {
     gulp.src('css/*.css')
         .pipe(connect.reload());
 });
+gulp.task('js', function () {
+    gulp.src('js/*.js')
+        .pipe(connect.reload());
+});
 
 gulp.task('watch', function () {
     gulp.watch(['*.html'], ['html']);
     gulp.watch(['css/*.css'], ['css']);
+    gulp.watch(['js/*.js'], ['js']);
 });
 
 gulp.task('default', ['connect', 'watch']);
