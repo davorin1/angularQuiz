d3b7acd09a297e09e676e2712f6f4e635e2ee1ca
Davorin <davorin.prislin@gmail.com>
2017-08-25 12:00:41 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: '',
+        livereload: true
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src('*.html')
+        .pipe(connect.reload());
+});
+gulp.task('css', function () {
+    gulp.src('css/*.css')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['*.html'], ['html']);
+    gulp.watch(['css/*.css'], ['css']);
+});
+
+gulp.task('default', ['connect', 'watch']);
